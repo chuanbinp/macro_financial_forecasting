b1fb70be9cf0ad9488a51dd2ee31152123d82475
chuanbinp <35805397+chuanbinp@users.noreply.github.com>
2025-11-24 18:01:31 -0500

    add df to parquet

--- applications/macro_financial_forecasting/src/utils/pydantic_parquet_util.py
+++ applications/macro_financial_forecasting/src/utils/pydantic_parquet_util.py
@@ -10,6 +10,10 @@ class PydanticParquetUtil:
         df.to_parquet(filename, index=False)
 
     @staticmethod
+    def save_to_parquet(df: pd.DataFrame, filename: str):
+        df.to_parquet(filename, index=False)
+
+    @staticmethod
     def load_from_parquet(filename: str, model_class: Type[BloombergNewsEntry]) -> list[BloombergNewsEntry]:
         df = pd.read_parquet(filename)
         return [model_class.parse_obj(row) for row in df.to_dict(orient="records")]
